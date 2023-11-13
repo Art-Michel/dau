bd2831acfe467f27d2317f99beaa97c70e328816
5Art <asheanx@gmail.com>
2023-11-13 18:00:08 +0100

    je sais pas mais ça compile

---
+++ src/Player.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdafx.h"
+#include "Player.h"
+#include "App/app.h"
+
+Player::Player(float x, float y)
+{
+	posX = x;
+	posY = y;
+	sprite = nullptr;
+}
+
+void Player::Init()
+{
+	sprite = App::CreateSprite("..\\Sprites\Square.png", 4, 4);
+	sprite->SetPosition(posX, posY);
+	sprite->SetScale(1.0f);
+}
+
+void Player::Update(float delta)
+{
+	sprite->Update(delta);
+}
---
+++ src/Player.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "App/SimpleSprite.h"
+
+class Player
+{
+public:
+	Player(float x, float y);
+
+	void Init();
+	void Update(float delta);
+
+private:
+	float posX;
+	float posY;
+	CSimpleSprite *sprite;
+};
